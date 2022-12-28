84e34999a9ed283513517effed602923bb0e124f
octanezdarelease <86353669+octanezdarelease@users.noreply.github.com>
2022-12-28 17:29:04 +0700

    Building Login Function: Add Vue Meta package

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,6 +1,8 @@
 <template>
+    <!-- Title -->
     <Head title="Login"/>
 
+    <!-- Main Section (OLD) -->
     <AuthenticationCard>
         <template #logo>
             <AuthenticationCardLogo/>
@@ -56,6 +58,106 @@
             </div>
         </form>
     </AuthenticationCard>
+
+    <!-- Main Section -->
+    <nav class="panel-menu" id="mobile-menu">
+        <ul>
+
+        </ul>
+        <div class="mm-navbtn-names">
+            <div class="mm-closebtn">
+                Close
+                <div class="tt-icon">
+                    <svg>
+                        <use xlink:href="#icon-cancel"></use>
+                    </svg>
+                </div>
+            </div>
+            <div class="mm-backbtn">Back</div>
+        </div>
+    </nav>
+    <main class="tt-offset-none" id="tt-pageContent">
+        <div class="container">
+            <div class="tt-loginpages-wrapper">
+                <div class="tt-loginpages">
+                    <a class="tt-block-title" href="index-2.html">
+                        <img alt="" src="images/logo.png">
+                        <div class="tt-title">
+                            Welcome to Forum19
+                        </div>
+                        <div class="tt-description">
+                            Log into your account to unlock true power of community.
+                        </div>
+                    </a>
+                    <form class="form-default">
+                        <div class="form-group">
+                            <label for="loginUserName">Username</label>
+                            <input class="form-control" id="loginUserName" name="name" placeholder="azyrusmax" type="text">
+                        </div>
+                        <div class="form-group">
+                            <label for="loginUserPassword">Password</label>
+                            <input class="form-control" id="loginUserPassword" name="name" placeholder="************"
+                                   type="password">
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <div class="form-group">
+                                    <div class="checkbox-group">
+                                        <input id="settingsCheckBox01" name="checkbox" type="checkbox">
+                                        <label for="settingsCheckBox01">
+                                            <span class="check"></span>
+                                            <span class="box"></span>
+                                            <span class="tt-text">Remember me</span>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col ml-auto text-right">
+                                <a class="tt-underline" href="#">Forgot Password</a>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <a class="btn btn-secondary btn-block" href="#">Log in</a>
+                        </div>
+                        <p>Or login with social network</p>
+                        <div class="row">
+                            <div class="col">
+                                <div class="form-group">
+                                    <a class="btn btn-color01 btn-secondary btn-block" href="#">
+                                        <i>
+                                            <svg class="icon">
+                                                <use xlink:href="#facebook-f-brands"></use>
+                                            </svg>
+                                        </i>
+                                        Facebook
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="form-group">
+                                    <a class="btn btn-color02 btn-block" href="#">
+                                        <i>
+                                            <svg class="icon">
+                                                <use xlink:href="#twitter-brands"></use>
+                                            </svg>
+                                        </i>
+                                        Twitter
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <p>Don’t have an account? <a class="tt-underline" href="#">Signup here</a></p>
+                        <div class="tt-notes">
+                            By Logging in, signing in or continuing, I agree to
+                            Forum19’s <a class="tt-underline" href="#">Terms of Use</a> and <a class="tt-underline"
+                                                                                               href="#">Privacy
+                            Policy.</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <script>
@@ -119,6 +221,23 @@ export default {
                 onFinish: () => this.form.reset('password')
             });
         }
+    },
+
+    // Include CSS & JS using Vue Meta
+    metaInfo: {
+        link: [
+            {
+                rel: "stylesheet",
+                href: "",
+            }
+        ],
+        script: [
+            {
+                src: "/hehe/test.js",
+                async: true,
+                defer: true
+            }
+        ],
     }
 }
 </script>
--- vite.config.js
+++ vite.config.js
@@ -17,4 +17,8 @@ export default defineConfig({
             },
         }),
     ],
+
+    commonjsOptions: {
+        esmExternals: true
+    },
 });
