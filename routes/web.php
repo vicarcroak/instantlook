c129e9d30bfe5b9929c07827d7572d4771f61d6b
octanezdarelease <86353669+octanezdarelease@users.noreply.github.com>
2023-01-03 10:20:52 +0700

    Format web.php

--- routes/web.php
+++ routes/web.php
@@ -24,11 +24,7 @@ Route::get('/', function () {
     ]);
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
+Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified',])->group(function () {
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
